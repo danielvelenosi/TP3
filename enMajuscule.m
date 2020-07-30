83f402e5a137387e6705e0b4d7b18d35632f2744
danielvelenosi <66074528+danielvelenosi@users.noreply.github.com>
2020-07-30 10:43:20 -0400

    Add files via upload

---
+++ enMajuscule.m
@@ -0,0 +1,12 @@
+function enMajuscule(chaine)
+% -------------------------------------------------------------------------
+% FICHIER       : enMajuscule.m
+% PAR           : Daniel Velenosi, Simon Tremblay, Daniele Sciascia et
+%                 Alvin Le
+% DATE          : 30/07/2020
+% DESCRIPTION   : Fonction qui reçoit une chaîne de caractère et retourne 
+%                 cette chaîne en majuscules.
+% -------------------------------------------------------------------------
+ fprintf('%s\n',upper(chaine));
+ 
+ end
