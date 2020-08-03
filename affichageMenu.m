c0740f9cadb43564704c3ee0c33d3a5f48b06706
alvinle207 <67423740+alvinle207@users.noreply.github.com>
2020-08-03 16:58:33 -0400

    Add files via upload

---
+++ affichageMenu.m
@@ -0,0 +1,18 @@
+function affichageMenu()
+%-------------------------------------------------------------------------------
+% Fonction qui affiche le menu
+%-------------------------------------------------------------------------------
+
+% Affichage du menu
+fprintf('**************** GESTIONNAIRE D''INVENTAIRE ****************');
+fprintf('\n1) Charger inventaire');
+fprintf('\n2) Afficher les caractéristiques d''une voiture');
+fprintf('\n3) Afficher les caractéristiques de toutes les voitures');
+fprintf('\n4) Ajouter un véhicule');
+fprintf('\n5) Supprimer un véhicule');
+fprintf('\n6) Modifier un véhicule');
+fprintf('\n7) Sauvegarder l''inventaire');
+fprintf('\n8) Afficher k meilleur');
+fprintf('\n9) Quitter');
+
+end
