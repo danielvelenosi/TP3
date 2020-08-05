9af3edefc0e45f1b1f9fad25c83e87d0d5af97c2
Sim-Tremblay <simon.tremblay.7@ens.etsmtl.ca>
2020-08-05 14:07:09 -0400

    update tp3

--- TP3.m
+++ TP3.m
@@ -19,28 +19,12 @@ function TP3()
 % addpath('Voiture');
 % addpath('Consommation');
 % addpath('Constantes');
-monInventaire = Inventaire();
+monInv = Inventaire();
 
 % Affichage du menu
 affichageMenu();
 choix = input('# ');
-% Gestion du menu
-% One of these below
-% 1) Charger l'inventaire
-%inventaire = lireData();
-% voiture = lireData();
 
-% Fonction Gestion des erreurs
-
-% Afficher le menu tant que le choix n’est pas de quitter le programme.
-
-% Lorsqu’on quitte, le programme propose de sauvegarder l’inventaire si une modification a été faite à l’inventaire.
-
-% Afficher k meilleure propose de sauvegarder les résultats dans un fichier texte.
-
-% Enregistrer les meilleures voitures
-% Fonction qui reçoit l’inventaire ainsi que le tableau des résultats obtenus par la fonction trouver les meilleures voitures.
-% Elle enregistre les résultats dans un fichier texte. Voir l’annexe pour avoir une idée du résultat attendu.
 
 while choix ~= 1
     fprintf('L''inventaire doit préalablement être chargé\n');
@@ -60,52 +44,51 @@ if choix == 1
         if choix ~= 1
             switch choix
                 case 2 % Afficher les caractéristiques d'une voiture
-                    fprintf('#2 \n');
-                    i = saisirChoix('Quelle voiture voulez-vous voir ? '); 
-                    disp(monInv(i))
+                    
+                    nbVoiture = saisirChoix('Quelle voiture voulez-vous voir ? '); 
+                    disp(monInv,nbVoiture)
                     affichageMenu()
                     
                 case 3 % Afficher les caractéristiques de toutes les voitures
-                    fprintf('#3 \n');
-                    disp(monInv.voiture)                  
-                    affichageMenu()
                     
-                case 4 % Ajouter un véhicule
-                    fprintf('#4 \n');
-                    nbVoiture = saisirChoix('Numéro d''identification: \n');
-                    marque = saisirChoix('Marque: \n');
-                    modele = saisirChoix('Modele: \n');
-                    annee = saisirChoix('Année: \n');
-                    combinee = saisirChoix('Consommation combinée: \n');
-                    ville = saisirChoix('Consommation en ville: \n');
-                    autoroute = saisirChoix('Consommation sur l''autoroute: \n');
-                    creerVoiture(monInv,nbVoiture,marque,modele,annee,combinee,ville,autoroute);
+                    disp(monInv)                  
                     affichageMenu()
+     
+                case 4 % Ajouter un véhicule
+                    
+                    nbVoiture = saisirChoix('Numéro d''identification: ');
+                    marque = saisirChoix('Marque: ');
+                    modele = saisirChoix('Modele: ');
+                    annee = saisirChoix('Année: ');
+                    combinee = saisirChoix('Consommation combinée: ');
+                    ville = saisirChoix('Consommation en ville: ');
+                    autoroute = saisirChoix('Consommation sur l''autoroute: ');
+                    creerVoiture(monInv,nbVoiture,marque,modele,annee,combinee,ville,autoroute);
+                    affichageMenu()                   
                     
                 case 5 % Supprimer un véhicule
-                    fprintf('#5 \n');
+                    
                     nbVoiture = saisirChoix('Quel voiture voulez-vous supprimer? ');
                     supprimerVoiture(monInv,nbVoiture);
                     fprintf('La voiture à bien été supprimée\n')
                     affichageMenu()
                     
                 case 6 % Modifier un véhicule
-                    fprintf('#6 \n');
+                    
                     nbVoiture = saisirChoix('Quelle voiture voulez-vous modifier? \n');
-                    i = saisirChoix('Quel attribut voulez-vous modifier? \n');
-                    disp(monInv(i))
-                    %choix = saisirChoix('Quel attribut voulez-vous modifier?',1,7);
+                    %i = saisirChoix('Quel attribut voulez-vous modifier? \n');
+                    %disp(monInv(i))
                     modifierVoiture(monInv,nbVoiture)
                     affichageMenu()
                     
                 case 7 % Sauvegarder l''inventaire
-                    fprintf('#7 \n');
+                    
                     sauvegardeMonInv(monInv)
                     fprintf('Le fichier à été enregistré');
                     affichageMenu()
                     
                 case 8 % Afficher k meilleur
-                    fprintf('#8 \n');
+                    
                     
                 case 9 % Quitter
                     choix_quit = saisirChoix('Voulez-vous sauvegarder avant de quitter? (O/N) ');
