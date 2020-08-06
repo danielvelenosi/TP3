827f342af972552671d6d534b696930d0e53c324
Danizard <daniele.sciascia.1@ens.etsmtl.ca>
2020-08-05 20:48:30 -0400

    Update Case 9 (Quitter)

--- TP3.m
+++ TP3.m
@@ -23,7 +23,7 @@ function TP3()
 
 
 % monInv = Inventaire();
-% 
+%
 % % Affichage du menu
 % affichageMenu();
 % choix = input('# ');
@@ -33,114 +33,117 @@ function TP3()
 %     fprintf('L''inventaire doit préalablement être chargé\n');
 %     affichageMenu();
 %     choix = saisirChoix('#');
-% end   
+% end
 
 %Initialisation des variables
 invCharger = false;
 sauvegarder = false;
 quit = false;
 
- while quit ~= 1
-       affichageMenu();
-       choix = str2num(saisirChoix('#'));
-  
-    switch choix
-%         while invCharger == false 
-%             fprintf('L''inventaire doit être préalablement chargé\n')
-
-
-                case 1 % Charger inventaire
-                monInv = Inventaire();
-                invCharger = true;
-                fprintf('L''inventaire à été chargé\n')
-
-
-                case 2 % Afficher les caractéristiques d'une voiture
-                    if invCharger == true
-                    nbVoiture = str2num(saisirChoix('Quelle voiture voulez-vous voir ? ')); 
-                    disp(monInv.voiture(nbVoiture))  
-                    else 
-                      fprintf('L''inventaire doit être préalablement chargé\n')  
-                    end
-
-
-                case 3 % Afficher les caractéristiques de toutes les voitures
-                    if invCharger == true
-                    disp(monInv) 
-                    else 
-                      fprintf('L''inventaire doit être préalablement chargé\n')  
-                    end
-                    
-
-
-                case 4 % Ajouter un véhicule
-                    if invCharger == true
-                    nbVoiture = numel(monInv.voiture) + 1;
-                    marque = saisirChoix('Marque: ');
-                    modele = saisirChoix('Modele: ');
-                    annee = saisirChoix('Année: ');
-                    combinee = str2num(saisirChoix('Consommation combinée: '));
-                    ville = str2num(saisirChoix('Consommation en ville: '));
-                    autoroute = str2num(saisirChoix('Consommation sur l''autoroute: '));
-                    creerVoiture(monInv,nbVoiture,marque,modele,annee,combinee,ville,autoroute);
-                    else 
-                      fprintf('L''inventaire doit être préalablement chargé\n')  
-                    end
-
-
-                case 5 % Supprimer un véhicule
-                    if invCharger == true
-                    nbVoiture = str2num(saisirChoix('Quel voiture voulez-vous supprimer? '));
-                    monInv.supprimerVoiture(nbVoiture);
-                    fprintf('La voiture à bien été supprimée\n')
-                    else 
-                      fprintf('L''inventaire doit être préalablement chargé\n')  
-                    end
-
-
-                case 6 % Modifier un véhicule
-                    if invCharger == true
-                    nbVoiture = str2num(saisirChoix('Quelle voiture voulez-vous modifier? '));
-                    disp(monInv.voiture(nbVoiture))
-                    monInv.modifierVoiture(nbVoiture)
-                    
-                    else 
-                      fprintf('L''inventaire doit être préalablement chargé\n')  
-                    end
-
-                case 7 % Sauvegarder l''inventaire
-                    if invCharger == true
-                    monInv.sauvegardeMonInv()
-                    sauvegarder = true;
-                    fprintf('Le fichier à été enregistré\n');
-                    else 
-                      fprintf('L''inventaire doit être préalablement chargé\n')  
-                    end
-
-
-                case 8 % Afficher k meilleur
-                    if invCharger == true
-                        
-                        
-                    else 
-                      fprintf('L''inventaire doit être préalablement chargé\n')  
-                    end  
-                        
-
-                case 9 % Quitter
-%                      quit = true;
-%                             choix_quit = saisirChoix('Voulez-vous sauvegarder avant de quitter? (O/N) ');
-%                             if choix_quit == O || choix_quit == o
-%                                 monInv.sauvegardeMonInv()
-%                            else
-    end 
- 
+while quit ~= 1
+    affichageMenu();
+    choix = str2num(saisirChoix('#'));
     
- end
-                 
+    switch choix
+        %         while invCharger == false
+        %             fprintf('L''inventaire doit être préalablement chargé\n')
+        
+        
+        case 1 % Charger inventaire
+            monInv = Inventaire();
+            invCharger = true;
+            fprintf('L''inventaire à été chargé\n')
+            
+            
+        case 2 % Afficher les caractéristiques d'une voiture
+            if invCharger == true
+                nbVoiture = str2num(saisirChoix('Quelle voiture voulez-vous voir ? '));
+                disp(monInv.voiture(nbVoiture))
+            else
+                fprintf('L''inventaire doit être préalablement chargé\n')
+            end
+            
+            
+        case 3 % Afficher les caractéristiques de toutes les voitures
+            if invCharger == true
+                disp(monInv)
+            else
+                fprintf('L''inventaire doit être préalablement chargé\n')
+            end
+            
+            
+            
+        case 4 % Ajouter un véhicule
+            if invCharger == true
+                nbVoiture = numel(monInv.voiture) + 1;
+                marque = saisirChoix('Marque: ');
+                modele = saisirChoix('Modele: ');
+                annee = saisirChoix('Année: ');
+                combinee = str2num(saisirChoix('Consommation combinée: '));
+                ville = str2num(saisirChoix('Consommation en ville: '));
+                autoroute = str2num(saisirChoix('Consommation sur l''autoroute: '));
+                creerVoiture(monInv,nbVoiture,marque,modele,annee,combinee,ville,autoroute);
+            else
+                fprintf('L''inventaire doit être préalablement chargé\n')
+            end
+            
+            
+        case 5 % Supprimer un véhicule
+            if invCharger == true
+                nbVoiture = str2num(saisirChoix('Quel voiture voulez-vous supprimer? '));
+                monInv.supprimerVoiture(nbVoiture);
+                fprintf('La voiture à bien été supprimée\n')
+            else
+                fprintf('L''inventaire doit être préalablement chargé\n')
+            end
+            
+            
+        case 6 % Modifier un véhicule
+            if invCharger == true
+                nbVoiture = str2num(saisirChoix('Quelle voiture voulez-vous modifier? '));
+                disp(monInv.voiture(nbVoiture))
+                monInv.modifierVoiture(nbVoiture)
+                
+            else
+                fprintf('L''inventaire doit être préalablement chargé\n')
+            end
+            
+        case 7 % Sauvegarder l''inventaire
+            if invCharger == true
+                monInv.sauvegardeMonInv()
+                sauvegarder = true;
+                fprintf('Le fichier à été enregistré\n');
+            else
+                fprintf('L''inventaire doit être préalablement chargé\n')
+            end
+            
+            
+        case 8 % Afficher k meilleur
+            if invCharger == true
+                
+                
+            else
+                fprintf('L''inventaire doit être préalablement chargé\n')
+            end
+            
+            
+        case 9 % Quitter
+            quit = true;
+            choix_quit = saisirChoix('Voulez-vous sauvegarder avant de quitter? (O/N) ');
+            if choix_quit == "O" || choix_quit == "o"
+                monInv.sauvegardeMonInv()
+            elseif choix_quit == "N" || choix_quit == "n"
+                
+            else 
+                gravite = 0;
+                gestionErreurs('Vous devez choisir entre Y et N', gravite);
+                
+            end
+    end
+    
 end
 %end
-        
+
 % else
 %     fprintf('L''inventaire doit préalablement être chargé\n');
 %     TP3();
