4fdc491bd5aa13a31ce56ba6179658d5a9a51c91
alvinle207 <67423740+alvinle207@users.noreply.github.com>
2020-08-05 21:04:56 -0400

    Add files via upload

---
+++ testCreerVoiture.m
@@ -0,0 +1,9 @@
+%On test la fonction creerVoiture
+
+% Initialisationj de monInv
+monInv = Inventaire();
+
+% On crée une voiture test
+creerVoiture(monInv, 22, 'Tesla','Y','2020',4,5,6)
+
+% On peut voir dans le Command Window que notre voiture a été créée.
